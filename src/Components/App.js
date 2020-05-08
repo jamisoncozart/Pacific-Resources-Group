b0ef7444b478235b9c9ae1b95ab117906d4193e1
Geoff Goetz <geoff.goetz503@gmail.com>
2020-05-08 16:20:47 -0700

    remove hamburgerMenu and SASS for further research, revert to navbar for time being

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import NavBar from './Shared/NavBar'
-import HamburgerMenu from './Menu/HamburgerMenu'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import '../App.css';
 
 function App() {
   return(
     <Router>
-      <HamburgerMenu />
+      <NavBar />
     </Router>
   )
 }
