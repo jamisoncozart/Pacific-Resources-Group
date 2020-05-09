3147ac2c1e71b4b07ee9571408150e31bcbf9132
Geoff Goetz <geoff.goetz503@gmail.com>
2020-05-08 17:06:19 -0700

    Add links to Header and Footer

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import NavBar from './Shared/NavBar'
+import NavBar from './Shared/NavBar';
+import Header from './Shared/Header';
+import Footer from './Shared/Footer';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import '../App.css';
 
 function App() {
   return(
     <Router>
+      <Header />
       <NavBar />
+      <Footer />
     </Router>
   )
 }
