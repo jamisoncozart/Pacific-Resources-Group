e4681c0eed49ea902abaf5879683f24ac490cf8f
Geoff Goetz <geoff.goetz503@gmail.com>
2020-05-15 09:23:28 -0700

    Continued research and trouble shooting Sass implementation

--- src/Components/Shared/NavBar.js
+++ src/Components/Shared/NavBar.js
@@ -11,14 +11,15 @@ function NavBar() {
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
       <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Placeholder</Nav.Link>
-      <NavDropdown title="Dropdown Placeholder" id="basic-nav-dropdown">
+      <Nav.Link href="#link">Who We Are</Nav.Link>
+      <Nav.Link href="#link">Contact Us</Nav.Link>
+      {/* <NavDropdown title="Dropdown Placeholder" id="basic-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Placeholder</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another Placeholder</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.3">Something Placeholder</NavDropdown.Item>
         <NavDropdown.Divider />
         <NavDropdown.Item href="#action/3.4">Separated Placeholder</NavDropdown.Item>
-      </NavDropdown>
+      </NavDropdown> */}
     </Nav>
     <Form inline>
       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
