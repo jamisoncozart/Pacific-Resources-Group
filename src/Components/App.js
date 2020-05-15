2b01118964f90e6529cce5dcbe84fc8acfce25e0
Geoff Goetz <geoff.goetz503@gmail.com>
2020-05-15 11:08:00 -0700

    Add routes

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 import NavBar from './Shared/NavBar';
 import Header from './Shared/Header';
 import Footer from './Shared/Footer';
+import Projects from './Projects/Projects';
+import Contacts from './Contacts/Contacts';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import '../App.css';
+import '../App.scss';
 
 function App() {
   return(
     <Router>
       <Header />
       <NavBar />
+
+      <Switch>
+        <Route path='/projects'>
+          <Projects />
+        </Route>
+        <Route path='/contacts'>
+          <Contacts />
+        </Route>
+      </Switch>
+
       <Footer />
     </Router>
   )
