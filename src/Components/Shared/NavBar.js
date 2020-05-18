450028f717973424272c20a9ececb224b8cf4102
Geoff Goetz <geoff.goetz503@gmail.com>
2020-05-18 11:18:39 -0700

    Add information text

--- src/Components/Shared/NavBar.js
+++ src/Components/Shared/NavBar.js
@@ -1,26 +1,29 @@
 import React from 'react';
-
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import logo from '../../Assets/PRG.png'
+import { Nav } from 'react-bootstrap';
 
 function NavBar() {
 
+  const styles = {
+    background: 'rgba(69, 81, 99, 0.9)',
+    color: 'white'
+  }
+  
+  const logoStyle = {
+    width: '5%',
+    height: '5%'
+  }
+
   return (
-<Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">Pacific Resources Group</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="/about">Who We Are</Nav.Link>
-      <Nav.Link href="/projects">What We Do</Nav.Link>
-      <Nav.Link href="/contacts">Contact Us</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
-  </Navbar.Collapse>
-</Navbar>
+    <div style={styles}>
+      <Nav className="mr-auto">
+        <img style={logoStyle} src={logo} alt='Business Logo'/>
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/about">Who We Are</Nav.Link>
+        <Nav.Link href="/projects">What We Do</Nav.Link>
+        <Nav.Link href="/contacts">Contact Us</Nav.Link>
+      </Nav>
+    </div>
   );
 }
 
