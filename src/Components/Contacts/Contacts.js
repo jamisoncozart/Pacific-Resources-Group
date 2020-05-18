45d5aadabc5096d52861a45b1f774f98ee63cea6
Geoff Goetz <geoff.goetz503@gmail.com>
2020-05-18 15:02:35 -0700

    Adjust styling for new background color

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Contacts() {
 
   const styles = {
-    color: 'white',
+    color: 'grey',
   }
 
   return(
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Projects() {
 
   const styles = {
-    color: 'white',
+    color: 'grey',
   }
 
   return(
