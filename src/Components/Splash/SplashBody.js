21c42fb2d0725f2ea4ef63b66e0d6f8e7fb0aade
Geoff Goetz <geoff.goetz503@gmail.com>
2020-05-19 11:13:32 -0700

    Add Carousel to component

--- src/Components/Splash/SplashBody.js
+++ src/Components/Splash/SplashBody.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Splash.scss'
+import ControlledCarousel from './Carousel'
 
 function SplashBody() {
   
@@ -7,6 +8,7 @@ function SplashBody() {
 
   return(
     <div className='splashWhole'>
+      <ControlledCarousel />
       <h1>Your Home Page could be here!</h1>
     </div>
   )
