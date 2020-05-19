595435e595173665efe1dd00b7498b959550b5ae
Geoff Goetz <geoff.goetz503@gmail.com>
2020-05-19 13:52:56 -0700

    Moved text from header to splash page

--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './Shared.scss'
+import './Shared.scss';
+import logo from '../../Assets/PRG.png';
 
 function Header() {
   return(
     <div className='headerWhole'>
-      <h1>Pacific Resources Group</h1>
-      <h3>Land Management | Urban Forestry | Natural Resource Consulting</h3>
+      <h1 id='title'>Pacific Resources Group</h1>
+      <img className='logoStyle' src={logo} alt='Business Logo'/>
     </div>
   );
 }
