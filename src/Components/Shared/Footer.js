58312b5354057b13b99d60ed97ef520f26d1e62a
Geoff Goetz <geoff.goetz503@gmail.com>
2020-05-18 17:00:07 -0700

    Replace some placeholders with data

--- src/Components/Shared/Footer.js
+++ src/Components/Shared/Footer.js
@@ -19,9 +19,12 @@ function Footer() {
           </Col>
 
           <Col>
-          <h5>Buisness Contact(Address?)</h5>
-          <h5>Buisness Contact(Email?)</h5>
-          <h5>Buisness Contact(Phone?)</h5>
+          <h5>
+            13688 SW Jenna Ct
+            Portland OR, 97223
+          </h5>
+          <h5>sfgoetz@frontier.com</h5>
+          <h5>(503) 222-4320</h5>
 
           </Col>
 
@@ -31,8 +34,9 @@ function Footer() {
             <p>Social Media 3</p>
           </Col>
           <Col xs lg="2">
-          <p>Privacy</p>
-          <p>Terms of Use</p>
+          <a href='#'>Privacy</a>
+          <br/>
+          <a href='#'>Terms of Use</a>
           </Col>
         </Row>
       </Container>
