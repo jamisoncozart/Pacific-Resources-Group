6ac794509d56e887538b3bf9f41727fbbc133cb3
Geoff Goetz <geoff.goetz503@gmail.com>
2020-05-19 16:56:27 -0700

    Complete refactor of header and footer

--- src/Components/Shared/Footer.js
+++ src/Components/Shared/Footer.js
@@ -1,45 +1,12 @@
 import React from 'react';
 import './Shared.scss'
-import { Container, Row, Col } from 'react-bootstrap';
+
 function Footer() {
   return(
     <div className='footerWhole'>
-      <Container>
-        <Row>
-          <Col>
-            <h3><strong>PRG</strong></h3>
-            <h5>Help us, Help you.</h5>
-          </Col>
-
-          <Col>
-          <p>Link</p>
-          <p>Link</p>
-          <p>Link</p>
-          <p>Link</p>
-          </Col>
-
-          <Col>
-          <h5>
-            13688 SW Jenna Ct
-            Portland OR, 97223
-          </h5>
-          <h5>sfgoetz@frontier.com</h5>
-          <h5>(503) 222-4320</h5>
-
-          </Col>
-
-          <Col md="auto">
-            <p>Social Media 1</p>
-            <p>Social Media 2</p>
-            <p>Social Media 3</p>
-          </Col>
-          <Col xs lg="2">
-          <a href='#'>Privacy</a>
-          <br/>
-          <a href='#'>Terms of Use</a>
-          </Col>
-        </Row>
-      </Container>
+      <div className='footer1'></div>
+      <div className='footer2'></div>
+      <div className='footer3'></div>
     </div>
   );
 }
--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './Shared.scss';
 import logo from '../../Assets/PRG.png';
+import { Button } from 'react-bootstrap';
 
 function Header() {
   return(
     <div className='headerWhole'>
-      <h1 id='title'>Pacific Resources Group</h1>
-      <img className='logoStyle' src={logo} alt='Business Logo'/>
+      <div className='headerBackground'>
+        <h1 id='title'>Pacific Resources Group<img className='logoStyle' src={logo} alt='Business Logo'/></h1>
+        <div className='buttons'>
+          <Button id='button1' href="/splash" variant="outline-light">Home</Button>
+          <Button id='button2' href="/projects" variant="outline-light">What We Do</Button>
+          <Button id='button3' href="/about" variant="outline-light">Who We Are</Button>
+          <Button id='button4' href="/contacts" variant="outline-light">Contact Us</Button>
+        </div>
+      </div>
     </div>
   );
 }
