cfc18c314663a894ea23b188e4b0cd891564053f
Geoff Goetz <geoff.goetz503@gmail.com>
2020-05-20 09:54:28 -0700

    Further refactor footer

--- src/Components/Shared/Footer.js
+++ src/Components/Shared/Footer.js
@@ -3,20 +3,19 @@ import './Shared.scss'
 
 function Footer() {
   return(
-    <div>
-      {/* <div className='textBlock1'>
-        <h3>Land Management</h3>
-      </div> */}
-      <div className='footerPics'>
-        <div className='footer1 text1'>
-          <h3>Land Management</h3>
-        </div>
-        <div className='footer2 text2'>
-          <h3>Land Management</h3>
-        </div>
-        <div className='footer3 text3'>
-          <h3>Land Management</h3>
-        </div>
+    <div className='footerPics'>
+      <div className='footer1'>
+        <h3 id='text1a'>Land</h3>
+        <h3 id='text1b'>Management</h3>
+      </div>
+      <div className='footer2'>
+        <h3 id='text2a'>Urban</h3>
+        <h3 id='text2b'>Forestry</h3>
+      </div>
+      <div className='footer3'>
+        <h3 id='text3a'>Natural</h3>
+        <h3 id='text3b'>Resource</h3>
+        <h3 id='text3c'>Consulting</h3>
       </div>
     </div>
   );
