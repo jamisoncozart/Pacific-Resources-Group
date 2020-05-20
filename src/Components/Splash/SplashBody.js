79448fe4c662024d0643cfcb49668f68598c62e1
Geoff Goetz <geoff.goetz503@gmail.com>
2020-05-20 14:53:44 -0700

    Add initial styling and text for splash component

--- src/Components/Splash/SplashBody.js
+++ src/Components/Splash/SplashBody.js
@@ -8,8 +8,11 @@ function SplashBody() {
 
   return(
     <div className='splashWhole'>
-      <h1>About Us</h1>
-
+      <h1 className='splashTitle'><span id='splashTitle1'>About</span> Us</h1>
+      <div>
+        <p id='caret1'>></p><p id='caret2'>></p>
+      </div>
+      
     </div>
   )
 }
