15509c921091b1285b85ff7dcf9acce01683501c
Geoff Goetz <geoff.goetz503@gmail.com>
2020-05-20 14:49:56 -0700

    refactor for routing fix

--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -8,7 +8,7 @@ function Header() {
     <div className='headerWhole'>
       <div className='headerBackground'>
       <div className='buttons'>
-          <Button id='button1' href="/splash" variant="outline-light">Home</Button>
+          <Button id='button1' href="/" variant="outline-light">Home</Button>
           <Button id='button2' href="/projects" variant="outline-light">What We Do</Button>
           <Button id='button3' href="/about" variant="outline-light">Who We Are</Button>
           <Button id='button4' href="/contacts" variant="outline-light">Contact Us</Button>
